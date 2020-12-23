b2d82ca8e5f5036477edb6cc6fc84e1c19696e68
marcus-elia <mselia@uvm.edu>
2020-12-22 20:40:15 -0500

    Added basic player shape

---
+++ Prime Number War/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    private float trackRadius = 5f;
+    private int smoothness = 24;
+    private float lineWidth = 0.1f;
+
+    public GameObject playerPrefab;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GeneratePlayer();
+    }
+
+    private void GeneratePlayer()
+    {
+        player = Instantiate(playerPrefab);
+        player.GetComponent<PlayerMesh>().CreateShape();
+        player.GetComponent<PlayerMesh>().SetLocation(new Vector3(-5, 0, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Prime Number War/Assets/Scripts/PlayerMesh.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMesh : MonoBehaviour
+{
+    private float radius = 1f;
+
+    private Color color = Color.black;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void CreateShape()
+    {
+        Vector2[] vertices2D = new Vector2[13];
+        for(int i = 0; i < 12; i++)
+        {
+            vertices2D[i] = new Vector2(radius * Mathf.Cos(i * Mathf.PI * 2 / 12), radius * Mathf.Sin(i * Mathf.PI * 2 / 12));
+        }
+        vertices2D[12] = new Vector2(0f, 0f);
+
+        Vector3[] vertices3D = System.Array.ConvertAll<Vector2, Vector3>(vertices2D, v => v);
+
+        int[] triangleIndices = new int[36];
+        
+        for(int i = 0; i < 11; i++)
+        {
+            triangleIndices[3 * i] = 12;
+            triangleIndices[3 * i + 1] = i;
+            triangleIndices[3 * i + 2] = i + 1;
+        }
+        triangleIndices[33] = 12;
+        triangleIndices[34] = 11;
+        triangleIndices[35] = 0;
+
+        Color[] vertexColors = new Color[13];
+        for(int i = 0; i < 13; i++)
+        {
+            vertexColors[i] = color;
+        }
+
+        Mesh mesh = new Mesh
+        {
+            vertices = vertices3D,
+            triangles = triangleIndices,
+            colors = vertexColors
+        };
+
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+
+        // Set up game object with mesh;
+        var meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        meshRenderer.material = new Material(Shader.Find("Sprites/Default"));
+
+        var filter = gameObject.AddComponent<MeshFilter>();
+        filter.mesh = mesh;
+    }
+
+
+    public void SetLocation(Vector3 inputLocation)
+    {
+        transform.position = inputLocation;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
