4045f246c036759457560762577b371d399f6094
marcus-elia <mselia@uvm.edu>
2020-12-29 10:26:37 -0500

    Start screen and instructions are now working

---
+++ Prime Number War/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play1()
+    {
+        SceneManager.LoadScene("Alone");
+    }
+
+    public void Play2()
+    {
+        SceneManager.LoadScene("VersusComputer");
+    }
+
+    public void GoToInstructions()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
